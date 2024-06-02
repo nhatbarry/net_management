b9e0019767aee9b094ab99a37229771983082d40
nhatbarry <nhatbarry@gmail.com>
2024-06-02 21:38:52 +0700

    fix name add

--- src/main/java/controller/ClientController.java
+++ src/main/java/controller/ClientController.java
@@ -2,8 +2,9 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
- 
+
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -14,60 +15,62 @@ import view.ClientView;
 public class ClientController {
     private ClientDao clientDao;
     private ClientView clientView;
+
     public ClientController(ClientView view) {
         this.clientView = view;
         clientDao = new ClientDao();
-        
+
         view.addAddClientListener(new AddClientListener());
         view.addEditClientListener(new EditClientListener());
         view.addDeleteClientListener(new DeleteClientListener());
         view.addClearListener(new ClearClientListener());
         view.addSortClientNameListener(new SortClientNameListener());
         view.addListClientSelectionListener(new ListClientSelectionListener());
+        view.addSearchListener(new SearchNameListener());
     }
 
-    public void showClientView(){
+    public void showClientView() {
         List<Client> clientList = clientDao.getListClients();
         clientView.setVisible(true);
         clientView.showListClients(clientList);
     }
 
-    class AddClientListener implements ActionListener{
+    class AddClientListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
             Client client = clientView.getClientInfo();
-            if (client != null){
+            if (client != null) {
                 clientDao.add(client);
                 clientView.showClient(client);
                 clientView.showListClients(clientDao.getListClients());
                 clientView.showMessage("Đã thêm");
             }
         }
-        
+
     }
 
-    class EditClientListener implements ActionListener{
+    class EditClientListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
             Client client = clientView.getClientInfo();
-            if (client != null){
+            if (client != null) {
                 clientDao.edit(client);
                 clientView.showClient(client);
                 clientView.showListClients(clientDao.getListClients());
                 clientView.showMessage("Đã cập nhật");
             }
         }
-        
+
     }
 
-    class DeleteClientListener implements ActionListener{
+    class DeleteClientListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
             Client client = clientView.getClientInfo();
-            if (client != null){
+            if (client != null) {
                 clientDao.delete(client);
                 clientView.clearClientInfo();
                 clientView.showListClients(clientDao.getListClients());
@@ -76,29 +79,54 @@ public class ClientController {
         }
     }
 
-    class ClearClientListener implements ActionListener{
+    class ClearClientListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
             clientView.clearClientInfo();
         }
-        
+
     }
 
-    class SortClientNameListener implements ActionListener{
+    class SortClientNameListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
             clientDao.sortClientByName();
             clientView.showListClients(clientDao.getListClients());
         }
-        
+
     }
 
-    class ListClientSelectionListener implements ListSelectionListener{
-        public void valueChanged(ListSelectionEvent e){
+    class SearchNameListener implements ActionListener {
+        private List<Client> clientList = clientDao.getListClients();
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String searchField = clientView.getSearchField();
+            List<Client> searchResults = new ArrayList<Client>();
+            boolean isFound = false;
+
+            for (Client client : clientList) {
+                if (client.getName().equals(searchField)) {
+                    searchResults.add(client);
+                    isFound = true;
+                } 
+            }
+            if (isFound) {
+                clientView.showListClients(searchResults);
+            }
+            else {
+                clientView.showMessage("Không tìm thấy");
+            }
+
+        }
+    }
+
+    class ListClientSelectionListener implements ListSelectionListener {
+        public void valueChanged(ListSelectionEvent e) {
             clientView.fillClientfromSelectedRow();
         }
     }
-    
+
 }
--- src/main/java/dao/ClientDao.java
+++ src/main/java/dao/ClientDao.java
@@ -1,5 +1,5 @@
 package dao;
-import java.io.File;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
